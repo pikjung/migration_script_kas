373c21b70a78096e621ed113b1d1062eb9c71089
pikjung <tanjungfikri1@gmail.com>
2022-07-14 16:39:47 +0700

    14/7/22 -w

--- ALL MIGRATION .sql
+++ ALL MIGRATION .sql
@@ -1,50 +1,50 @@
 -- c_bankstatement
-ALTER TABLE c_bankstatement ADD COLUMN c_project_id NUMERIC(10,0);
+ALTER TABLE IF EXISTS c_bankstatement ADD COLUMN IF NOT EXISTS c_project_id NUMERIC(10,0) NULL DEFAULT NULL:numeric;
 -- c_bankstatementline
-ALTER TABLE c_bankstatementline ADD COLUMN c_project_id NUMERIC(10,0);
+ALTER TABLE IF EXISTS c_bankstatementline ADD COLUMN IF NOT EXISTS c_project_id NUMERIC(10,0) NULL DEFAULT NULL:numeric;
 -- c_bpartner_location
-ALTER TABLE c_bpartner_location ADD COLUMN name CHARACTER VARYING(300) DEFAULT '.';
+ALTER TABLE IF EXISTS c_bpartner_location ADD COLUMN IF NOT EXISTS name CHARACTER VARYING(300) NOT NULL DEFAULT '.'::character varying;
 -- c_dunning_header_v
-ALTER TABLE c_dunning_header_v ADD COLUMN c_bpartner_location_name CHARACTER VARYING(300);
+ALTER TABLE IF EXISTS c_dunning_header_v ADD COLUMN IF NOT EXISTS c_bpartner_location_name CHARACTER VARYING(300) NULL;
 -- c_dunning_header_vt
-ALTER TABLE c_dunning_header_vt ADD COLUMN c_bpartner_location_name CHARACTER VARYING(300);
+ALTER TABLE IF EXISTS c_dunning_header_vt ADD COLUMN IF NOT EXISTS c_bpartner_location_name CHARACTER VARYING(300) NULL;
 --  c_invoice
-ALTER TABLE c_invoice ADD COLUMN approveddiscby CHARACTER VARYING(255);
+ALTER TABLE IF EXISTS c_invoice ADD COLUMN IF NOT EXISTS approveddiscby CHARACTER VARYING(255) NULL;
 --  c_invoice_header_v
-ALTER TABLE c_invoice_header_v ADD COLUMN bp_location_name CHARACTER VARYING(300);
+ALTER TABLE IF EXISTS c_invoice_header_v ADD COLUMN IF NOT EXISTS bp_location_name CHARACTER VARYING(300) NULL;
 -- c_invoice_header_vt
-ALTER TABLE c_invoice_header_vt ADD COLUMN bp_location_name CHARACTER VARYING(300);
+ALTER TABLE IF EXISTS c_invoice_header_vt ADD COLUMN IF NOT EXISTS bp_location_name CHARACTER VARYING(300) NULL;
 -- c_invoicetax
-ALTER TABLE c_invoicetax ADD COLUMN taxnbr CHARACTER VARYING(22);
+ALTER TABLE IF EXISTS c_invoicetax ADD COLUMN IF NOT EXISTS taxnbr CHARACTER VARYING(22) NULL DEFAULT NULL:character varying;
 -- c_order_header_v
-ALTER TABLE c_order_header_v ADD COLUMN bp_location_name CHARACTER VARYING(300);
-ALTER TABLE c_order_header_v ADD COLUMN bill_bp_location_name CHARACTER VARYING(300);
+ALTER TABLE IF EXISTS c_order_header_v ADD COLUMN IF NOT EXISTS bp_location_name CHARACTER VARYING(300) NULL;
+ALTER TABLE IF EXISTS c_order_header_v ADD COLUMN IF NOT EXISTS bill_bp_location_name CHARACTER VARYING(300) NULL;
 -- c_order_header_vt
-ALTER TABLE c_order_header_vt ADD COLUMN bp_location_name CHARACTER VARYING(300);
-ALTER TABLE c_order_header_vt ADD COLUMN bill_bp_location_name CHARACTER VARYING(300);
+ALTER TABLE IF EXISTS c_order_header_vt ADD COLUMN IF NOT EXISTS bp_location_name CHARACTER VARYING(300) NULL;
+ALTER TABLE IF EXISTS c_order_header_vt ADD COLUMN IF NOT EXISTS bill_bp_location_name CHARACTER VARYING(300) NULL;
 -- c_project_header_v
-ALTER TABLE c_project_header_v ADD COLUMN c_bp_location_name CHARACTER VARYING(300);
+ALTER TABLE IF EXISTS c_project_header_v ADD COLUMN IF NOT EXISTS c_bp_location_name CHARACTER VARYING(300) NULL;
 -- c_project_header_vt
-ALTER TABLE c_project_header_vt ADD COLUMN c_bp_location_name CHARACTER VARYING(300);
+ALTER TABLE IF EXISTS c_project_header_vt ADD COLUMN IF NOT EXISTS c_bp_location_name CHARACTER VARYING(300) NULL;
 -- isy_timesheet
 CREATE TABLE isy_timesheet (
-    ad_client_id NUMERIC(10,0),
-    ad_org_id NUMERIC(10,0),
-    ad_orgtrx_id NUMERIC(10,0),
-    c_activity_id NUMERIC(10,0),
-    c_bpartner_id NUMERIC(10,0),
-    c_campaign_id NUMERIC(10,0),
-    c_project_id NUMERIC(10,0),
-    created TIMESTAMP WITHOUT TIME ZONE,
+    ad_client_id NUMERIC(10,0) NOT NULL,
+    ad_org_id NUMERIC(10,0) NOT NULL,
+    ad_orgtrx_id NUMERIC(10,0) NULL,
+    c_activity_id NUMERIC(10,0) NULL,
+    c_bpartner_id NUMERIC(10,0) NOT NULL,
+    c_campaign_id NUMERIC(10,0) NULL,
+    c_project_id NUMERIC(10,0) NULL,
+    created TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT statement_timestamp(),
     createdby NUMERIC,
-    endtime TIMESTAMP WITHOUT TIME ZONE,
-    isactive CHARACTER(1) DEFAULT 'Y',
-    isy_timesheet_id NUMERIC(10,0),
+    endtime TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT statement_timestamp(),
+    isactive CHARACTER(1) DEFAULT 'Y'::bpchar,
+    isy_timesheet_id NUMERIC(10,0) NOT NULL NOT NULL DEFAULT statement_timestamp(),
     starttime TIMESTAMP WITHOUT TIME ZONE,
     status CHARACTER VARYING(60),
-    updated TIMESTAMP WITHOUT TIME ZONE,
-    updatedby NUMERIC(10,0),
-    user1_id NUMERIC(10,0),
-    user2_id NUMERIC(10,0),
+    updated TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT statement_timestamp(),
+    updatedby NUMERIC(10,0) NOT NULL,
+    user1_id NUMERIC(10,0) NOT NULL,
+    user2_id NUMERIC(10,0) NOT NULL,
 );
 
